fa4a457c000e9cd4352436630c163f9713f44a89
Mi hich x <Misha777px@gmail.com>
2025-12-05 17:02:22 +0400

    Update

--- assets/enroll.js
+++ assets/enroll.js
@@ -1,13 +1,27 @@
 elem = document.querySelector(".modal-bg");
 elem1 = document.querySelector(".modal");
 elem2 = document.querySelector(".modal-good");
+elem3 = document.querySelector(".dropdown-content");
+elem4 = document.querySelector(".modal-bg-catalog");
+
+let sendTimeout;
+
+function open_catalog() {
+    elem3.classList.add("display");
+    elem4.classList.add("display");
+    setTimeout(() => {
+        elem3.classList.add("show");
+    }, 10);
+}
 
 function send() {
     elem1.classList.remove("display");
     elem2.classList.add("display");
-    setTimeout(() => elem2.classList.remove("display"), 1500);
-    setTimeout(() => elem.classList.remove("display"), 1500);
-    return false; // Предотвращает отправку формы
+    if (sendTimeout) clearTimeout(sendTimeout);
+    sendTimeout = setTimeout(() => {
+        elem2.classList.remove("display");
+        elem.classList.remove("display");
+    }, 1500);
 }
 
 function modal() {
@@ -19,4 +33,12 @@ function modal() {
 function closing() {
     elem.classList.remove("display");
     elem1.classList.remove("display");
+}
+
+function closing_catalog() {
+    elem3.classList.remove("show");
+    setTimeout(() => {
+        elem3.classList.remove("display");
+        elem4.classList.remove("display");
+    }, 300);
 }
