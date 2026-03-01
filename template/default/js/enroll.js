67e6222394ab46dc863e2696de3daa45347407e9
Mi hich x <Misha777px@gmail.com>
2026-03-01 14:34:35 +0400

    Исправил enroll.js теперь переменная modal = modal0

--- template/default/js/enroll.js
+++ template/default/js/enroll.js
@@ -1,11 +1,11 @@
-modal = document.querySelector(".modal");
-modal1 = document.querySelector(".modal1");
-modal2 = document.querySelector(".modal2");
-modal_bg = document.querySelector(".modal-bg");
-modal_bg_catalog = document.querySelector(".modal-bg-catalog");
-dropdown_content = document.querySelector(".dropdown-content");
-aboutus_img = document.querySelector(".aboutus-img");
-error = document.querySelector(".error");
+const modal0 = document.querySelector(".modal");
+const modal1 = document.querySelector(".modal1");
+const modal2 = document.querySelector(".modal2");
+const modal_bg = document.querySelector(".modal-bg");
+const modal_bg_catalog = document.querySelector(".modal-bg-catalog");
+const dropdown_content = document.querySelector(".dropdown-content");
+const aboutus_img = document.querySelector(".aboutus-img");
+const error = document.querySelector(".error");
 
 function open_catalog() {
     dropdown_content.classList.add("display");
@@ -27,17 +27,17 @@ function closing_catalog() {
 
 function send() {
     modal_bg.classList.remove("active");
-    modal.classList.remove("active");
+    modal0.classList.remove("active");
 }
 
 function modal() {
     modal_bg.classList.add("active");
-    modal.classList.add("active");
+    modal0.classList.add("active");
 }
 
 function closing() {
     modal_bg.classList.remove("active");
-    modal.classList.remove("active");
+    modal0.classList.remove("active");
     modal1.classList.remove("active");
     modal2.classList.remove("active");
     error.classList.add("delate")
